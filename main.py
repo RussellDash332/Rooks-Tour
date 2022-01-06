ba459b578b778a6ee3331ac533528c4a90fc39f7
RussellDash332 <russellsaerang@gmail.com>
2022-01-07 00:14:27 +0800

    Modify rules and add credit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Rook's Tour
+# Russell Saerang and Darren Lionardo
+
 def game():
   board = [[1, 0, 0, 0],
           [0, 0, 1, 0],
@@ -21,8 +24,9 @@ def game():
                 [0, 0, 0, 0],
                 [0, 0, 0, 1]]]
 
-  max_rounds = 15
+  max_rounds = 8
   curr_round = 1
+  check_three_collinear = False
 
   teams = [[0,0],
           [1,2],
@@ -38,26 +42,22 @@ def game():
     for team in range(1,5):
       while True:
         while True:
-          temp_x = input(f"Team {team}: Where do you want to go (x)? ")
-          if temp_x in list(map(str, range(4))):
-            temp[team-1][0] = int(temp_x)
-            break
-          else:
-            print("Not a valid x input! Try again :(")
-
-        while True:
-          temp_y = input(f"Team {team}: Where do you want to go (y)? ")
-          if temp_y in list(map(str, range(4))):
-            temp[team-1][1] = int(temp_y)
-            break
-          else:
-            print("Not a valid y input! Try again :(")
-          
+          try:
+            pos = int(input(f"Team {team}: Where do you want to go (1-16)? "))
+            if pos in range(1, 17):
+              temp[team-1][0], temp[team-1][1] = (pos - 1) // 4, (pos - 1) % 4
+              break
+            else:
+              print(f"Team {team}: Input not in the correct range! Try again :(")
+          except:
+            print(f"Team {team}: Not a valid input! Try again :(")
+        
         # Check for rook move
-        if (int(temp_x) == teams[team-1][0]) ^ (int(temp_y) == teams[team-1][1]):
+        if (temp[team-1][0] == teams[team-1][0]) ^ (temp[team-1][1] == teams[team-1][1]):
+          print(f"Team {team} wants to go to {pos}")
           break
         else:
-          print("Not a rook move! Try again :(")
+          print(f"Team {team}: Not a rook move! Try again :(")
 
       if visited[team-1][temp[team-1][0]][temp[team-1][1]] == 1: # visited previous spot
         eligible[team-1] = False
@@ -90,30 +90,31 @@ def game():
     # Only one eligible remaining
     game_ends = sum(eligible) <= 1
     
-    # Check for row
-    if not game_ends:
-      for i in range(4):
-        if sum(board[i]) >= 3:
+    if check_three_collinear:
+      # Check for row
+      if not game_ends:
+        for i in range(4):
+          if sum(board[i]) >= 3:
+            game_ends = True
+      
+      # Check for column
+      if not game_ends:
+        for i in range(4):
+          result = 0
+          for j in range(4):
+            result += board[j][i]
+          if result >= 3:
+            game_ends = True
+      
+      # Check for diagonal
+      if not game_ends:
+        diag1 = 0
+        diag2 = 0
+        for i in range(4):
+          diag1 += board[i][i]
+          diag2 += board[i][3-i]
+        if diag1 >= 3 or diag2 >= 3:
           game_ends = True
-    
-    # Check for column
-    if not game_ends:
-      for i in range(4):
-        result = 0
-        for j in range(4):
-          result += board[j][i]
-        if result >= 3:
-          game_ends = True
-    
-    # Check for diagonal
-    if not game_ends:
-      diag1 = 0
-      diag2 = 0
-      for i in range(4):
-        diag1 += board[i][i]
-        diag2 += board[i][3-i]
-      if diag1 >= 3 or diag2 >= 3:
-        game_ends = True
 
     max_rounds = int(max_rounds) # for prettifying float back to int
     print()
@@ -137,20 +138,5 @@ def game():
     print("-"*30)
 
   print("Game ended!")
-
-"""
-0 2 1 3 2 0 3 1
-0 3 0 3 2 1 3 0 -> ends in 2 turns because Team 1 and Team 2 meet each other
-
-0.0.1.2.2.1.3.3
-0 2 1 3 2 0 3 1
-0 3 2 3 2 1 3 2
-0 2 0 3 2 2 3 3 -> Team 1 gets 0 points for travelling back
-
-0.0.1.2.2.1.3.3
-0 2 1 3 2 0 3 1
-0 3 0 3 2 1 3 2
-0 1 2 3 2 2 3 0
-"""
 
 game()
